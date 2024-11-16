4aa413208ef091841dbe82184ba99d025382f272
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-11-15 22:39:12 -0800

    added fragments and linked each one to the corresponding navigation buttons

--- app/src/main/java/com/example/tasksitinerary/MainActivity.kt
+++ app/src/main/java/com/example/tasksitinerary/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.tasksitinerary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,10 +17,38 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        replaceFragment(Home())
         ViewCompat.setOnApplyWindowInsetsListener( binding.main ) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId) {
+
+                R.id.home -> replaceFragment(Home())
+                R.id.add_task -> replaceFragment(AddTask())
+                R.id.calendar -> replaceFragment(Calendar())
+
+                else -> {
+
+                }
+            }
+
+            true
+
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+
+
     }
 }
